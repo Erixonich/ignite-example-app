fdf562cb89b866014a0c86523e91685b60e9e7fb
Eduard Rakhmankulov <erixonich@gmail.com>
2023-05-17 12:13:13 +0300

    mapReduce problem solution baseline

--- src/main/java/com/altuera/ignite/Main.java
+++ src/main/java/com/altuera/ignite/Main.java
@@ -3,13 +3,17 @@ package com.altuera.ignite;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.configuration.ClientConfiguration;
 
 import java.util.UUID;
 
 public class Main {
     static int SIZE = 100_000;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Ignite ign = Ignition.start();
+
+        IgniteClient igniteClient = Ignition.startClient(new ClientConfiguration().setAddresses("127.0.0.1:10800"));
 
         IgniteCache<UUID, Person> personCache = ign.createCache("Person");
 
@@ -19,8 +23,8 @@ public class Main {
         }
 
 
-        ? result = ign.compute().execute(PersonAverageAgeTask.class, ?);
+        Double result = ign.compute().<Void, Double>execute(PersonAverageAgeTask.class, null);
 
-        System.out.println(result);
+        System.out.println("Average person = " + result);
     }
 }
--- src/main/java/com/altuera/ignite/PersonAverageAgeTask.java
+++ src/main/java/com/altuera/ignite/PersonAverageAgeTask.java
@@ -2,6 +2,7 @@ package com.altuera.ignite;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteException;
+import org.apache.ignite.cache.CachePeekMode;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.compute.ComputeJob;
 import org.apache.ignite.compute.ComputeJobAdapter;
@@ -11,24 +12,50 @@ import org.apache.ignite.resources.IgniteInstanceResource;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.cache.Cache;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Посчитать средний возраст все персон в кеше Person
  */
-public class PersonAverageAgeTask extends ComputeTaskAdapter<?,?> {
+//select avg(age) from persons
+    //where FirstName = 'Maria'
+public class PersonAverageAgeTask extends ComputeTaskAdapter<Void,Double> {
     @Override
-    public @NotNull Map<? extends ComputeJob, ClusterNode> map(List<ClusterNode> subgrid, @Nullable Object arg) throws IgniteException {
-        return null;
+    public @NotNull Map<? extends ComputeJob, ClusterNode> map(List<ClusterNode> subgrid, @Nullable Void arg) throws IgniteException {
+        Map<ComputeJob, ClusterNode> res = new HashMap<>();
+
+        for (ClusterNode clusterNode : subgrid) {
+            res.put(new PersonAverageAgeJob(), clusterNode);
+        }
+
+        return res;
     }
 
     @Nullable
     @Override
-    public ? reduce(List<ComputeJobResult> results) throws IgniteException {
-        return null;
+    public Double reduce(List<ComputeJobResult> results) throws IgniteException {
+        long sumYears = 0;
+        long personCount = 0;
+        for (ComputeJobResult res : results) {
+            AgeInfo ageInfo = res.<AgeInfo>getData();
+
+            sumYears += ageInfo.sumYears;
+            personCount += ageInfo.personCount;;
+        }
+
+        return 1.0 * sumYears / personCount;
     }
 
+    private static class AgeInfo {
+        long sumYears = 0;
+        long personCount = 0;
+    }
 
     public static class PersonAverageAgeJob extends ComputeJobAdapter {
         @IgniteInstanceResource
@@ -36,7 +63,15 @@ public class PersonAverageAgeTask extends ComputeTaskAdapter<?,?> {
 
         @Override
         public Object execute() throws IgniteException {
-            return null;
+            AgeInfo ageInfo = new AgeInfo();
+
+            LocalDate now = LocalDate.now();
+            for (Cache.Entry<UUID, Person> e : ign.<UUID, Person>cache("Person").localEntries(CachePeekMode.PRIMARY)) {
+                ageInfo.personCount++;
+                ageInfo.sumYears += ChronoUnit.YEARS.between(e.getValue().getBirthday(), now);
+            }
+
+            return ageInfo;
         }
     }
 }
